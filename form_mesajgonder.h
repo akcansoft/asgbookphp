3527457e10bb7e55fa9cba95989385e6cd232d06
akcanSoft <makcan@gmail.com>
2019-11-03 15:52:36 +0300

    Update form_mesajgonder.h
    Güncelleme v2.5

--- form_mesajgonder.h
+++ form_mesajgonder.h
@@ -1,16 +1,16 @@
-﻿<script type='text/javascript' src='form.js'></script>
+<script type='text/javascript' src='form.js'></script>
 <form method="POST" name="form1" onSubmit="return formGonder()">
 <p class="baslik">#BASLIK#</p>
 <table border="0">
-<tr><td>Adı Soyadı: <font color=red>*</font></td><td><input id="adsoyad" name="name" type="text" value="#ADI#"></td></tr>
+<tr><td>Adı Soyadı: <span style="color=red">*</span></td><td><input id="adsoyad" name="name" type="text" value="#ADI#"></td></tr>
 <tr><td>Email:</td><td><input type="email" name="email" value="#EMAIL#"></td></tr>
 <tr><td>Web:</td><td><input type="url" name="web" value="#WEB#"></td></tr>
 <tr><td>Memleket:</td><td><input type="text" name="yer" value="#YER#"></td></tr>
 <tr><td>&nbsp;</td>
 <td>Kalan harf:
-  <input type="text" size="4" name="gen" style="background-color: #FFFFFF; border: none" readonly /></td>
+ <input type="text" size="4" name="gen" style="background-color: #FFF; border: none" readonly /></td>
 </tr>
-<tr><td valign="top"><p>Mesaj:  <font color=red>*</font><p>
+<tr><td valign="top"><p>Mesaj: <span style="color=red">*</span><p>
 <p style="font-size:8pt; color:#888">İzin verilen<br>
 HTML etiketleri:<br>
 #HTMLTAGS#</p></td>
